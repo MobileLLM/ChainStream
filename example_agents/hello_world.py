40e96aa83dcb76024c30b7d1ffb5ebc977f91874
Yuanchun Li <ylimit@qq.com>
2024-01-22 18:21:43 +0800

    add simple example agents

---
+++ example_agents/hello_world.py
@@ -0,0 +1,27 @@
+import chainstream as cs
+
+
+class HelloAgent(cs.agent.Agent):
+    """
+    A simple agent that says hello to people in front camera
+    """
+    def __init__(self):
+        self._source1 = cs.get_stream('front_camera_video_01')  # instance of Stream
+        self._llm = cs.llm.get_llm('GPT4V')
+        self.video_buffer = cs.context.VideoBuffer(duration=10)
+        self.has_people = cs.create_stream('front_camera_has_people')
+
+    def start(self):
+        def handle_new_frame(frame):
+            self.video_buffer.save(frame)
+            prompt = cs.llm.make_prompt([frame, 'is there a person in the image? Simply answer Yes or No'])
+            response = self._llm.query(prompt).lower().strip()
+            if response.startswith('yes'):
+                self.has_people.send_item({'message': 'hello', 'frame': frame})
+
+    def pause(self):
+        self._source1.pause_listener(self)
+
+    def stop(self):
+        self._source1.remove_listener(self)
+
