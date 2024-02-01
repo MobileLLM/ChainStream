d189823087940d1071c8dea87091caa567a2dd21
gitdong <1583761806@qq.com>
2024-02-01 10:43:20 +0800

    修改了clothing_recommendation

--- agents/user_agents/clothing_recommendation.py
+++ agents/user_agents/clothing_recommendation.py
@@ -3,16 +3,17 @@ import chainstream as cs
 
 class ClothingRecommendation(cs.agent.Agent):
     """
-       每天早上结合用户今天的日程内容和当地的天气情况给用户推荐衣物
-       包括查找天气、读取日程、推荐衣服 
+       每天早上当用户醒来后，结合用户今天的日程内容和当地的天气情况给用户推荐衣物
     """
     def __init__(self):
         self._source2 = cs.get_stream('schedule_stream')
         self._source1 = cs.get_stream('weather_stream')
+        self._source3 = cs.get_stream('wakeup_stream')          # wakeup stream，由一个专门负责检测起床的 agent 来发布事件
         self._llm = cs.llm.get_llm('ChatGPT')
         self.schedule_buffer = cs.context.TextBuffer()
         self.weather_buffer = cs.context.TextBuffer()
-        self.send_recommend_info = cs.action.timed_reminder()
+        self.wakeup_buffer = cs.context.TextBuffer()
+        self.send_recommend_info = cs.action.message_reminder()
         self.clothing_recommendation = cs.create_stream('clothing_recommendation')
 
     def start(self):
@@ -22,17 +23,23 @@ class ClothingRecommendation(cs.agent.Agent):
 
         def handle_new_weather(weather):
             self.weather_buffer.save(weather)
+
+        def handle_new_wakeup(wakeup):
+            self.wakeup_buffer.save(wakeup)
             prompt = cs.llm.make_prompt([self.schedule_buffer, self.weather_buffer, 'clothes recommended to wear'])
-            response = self._llm.query(prompt)      #
-            self.send_recommend_info({'message': response, 'time': '7:00,AM'})
+            response = self._llm.query(prompt)  #
+            self.send_recommend_info({'message': response})
 
         self._source1.register_listener(self, handle_new_weather)
         self._source2.register_listener(self, handle_new_schedule)
+        self._source3.register_listener(self, handle_new_wakeup)
 
     def pause(self):
         self._source1.pause_listener(self)
         self._source2.pause_listener(self)
+        self._source3.pause_listener(self)
 
     def stop(self):
         self._source1.remove_listener(self)
         self._source2.remove_listener(self)
+        self._source3.remove_listener(self)
