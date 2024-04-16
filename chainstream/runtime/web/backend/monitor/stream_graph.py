f6d7fc3abc1471ccaa25777c287b96d454ca79eb
liou <1127429568@qq.com>
2024-04-16 23:56:18 +0800

    fix bug

--- chainstream/runtime/web/backend/monitor/stream_graph.py
+++ chainstream/runtime/web/backend/monitor/stream_graph.py
@@ -6,7 +6,5 @@ stream_graph_blueprint = Blueprint('stream_graph_blueprint', __name__)
 
 @stream_graph_blueprint.route('/api/monitor/streamGraph', methods=['GET'])
 def get_stream_graph():
-    # print("Getting stream graph statistics")
     node, edge = chainstream_core.get_graph_statistics()
-    print(node, edge)
     return jsonify({"node": node, "edge": edge})
