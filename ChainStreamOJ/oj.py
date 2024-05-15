784c187ff07bbbe5a6ff4d97197b34ed84be01ee
liou <1127429568@qq.com>
2024-05-15 22:14:11 +0800

    暂存

--- ChainStreamOJ/oj.py
+++ ChainStreamOJ/oj.py
@@ -1,8 +1,14 @@
 from chainstream.runtime import RuntimeCore
 
 class OJ:
-    def __init__(self):
+    def __init__(self, task_file, agent_file):
         self.runtime = RuntimeCore()
+        self.task_file = task_file
+        self.agent_file = agent_file
+    
+    def start_test_agent(self):
+        pass
 
-    def 
+    def _init_task(self):
+        self._read_task()
         
