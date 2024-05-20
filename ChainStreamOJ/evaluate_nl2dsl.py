49f0b0b57bb3401a95fb819997fc41b0075e3ef9
liou <1127429568@qq.com>
2024-05-20 15:58:00 +0800

    add evaluate_nl2dsl code

---
+++ ChainStreamOJ/evaluate_nl2dsl.py
@@ -0,0 +1,23 @@
+from .oj import OJ
+from ..NL2DSL.nl2dsl import NL2DSL
+from tasks import ALL_TASKS
+import json
+
+def evaluate_nl2dsl():
+    all_results = []
+    for task in ALL_TASKS:
+        task_description = task.task_description
+        agent = NL2DSL(task_description)
+        oj = OJ(task, agent)
+        res = oj.start_test_agent()
+        all_results.append(res)
+        json.dump(res, open(f"./results/{task.task_name}.json", "w"))
+    return all_results
+
+if __name__ == '__main__':
+    all_results = evaluate_nl2dsl()
+    print(all_results)
+
+
+
+
---
+++ ChainStreamOJ/tasks/__init__.py
@@ -0,0 +1,7 @@
+# from task_example/*/config.py import all tasks and put them in a list
+
+ALL_TASKS = []
+
+# import all tasks
+from task_example.config import task_example
+ALL_TASKS.append(task_example)
