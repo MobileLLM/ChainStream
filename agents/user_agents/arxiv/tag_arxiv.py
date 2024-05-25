4b133b69269788d30a9086a49eee66d16691f9a3
liou <1127429568@qq.com>
2024-05-25 18:51:43 +0800

    Simplified tag agent, overall looks good. the next step is to try running these agents

--- agents/user_agents/arxiv/tag_arxiv.py
+++ agents/user_agents/arxiv/tag_arxiv.py
@@ -21,33 +21,13 @@ class TagArxiv(Agent):
             base_tag_prompt = ("Give you an abstract of a paper: %s . What tag would you like to add to this paper? Choose from "
                                "the following: ") % paper_content
             topic_tags = ['Artificial Intelligence', 'Computer Vision and Pattern Recognition', 'Machine Learning', 'Neural and Evolutionary Computing', 'Robotics', 'Graphics', 'Human-Computer Interaction', 'Multiagent Systems', 'Software Engineering','Other']
-            approch_tags = ['Theoretical Research', 'Experimental Research', 'Simulation and Modeling', 'Empirical Research','Case Studies','Other']
-            problems_tags = ['Optimization', 'Classification', 'Regression', 'Clustering', 'Generation','Other']
-            implementation_tags = ['Software', 'Hardware', 'Hybrid', 'System Integration','Other']
-            algorithms_tags = ['Deep Learning', 'Machine Learning', 'Classical', 'Heuristic','Evolutionary','Other']
-            stage_tags = ['Conceptual', 'Development', 'Testing', 'Deployment', 'Maintenance','Other']
 
             prompt_topic = base_tag_prompt + ", ".join(topic_tags)
-            prompt_approch = base_tag_prompt + ", ".join(approch_tags)
-            prompt_problems = base_tag_prompt + ", ".join(problems_tags)
-            prompt_implementation = base_tag_prompt + ", ".join(implementation_tags)
-            prompt_algorithms = base_tag_prompt + ", ".join(algorithms_tags)
-            prompt_stage = base_tag_prompt + ", ".join(stage_tags)
 
             topic_tag = self.llm.query(prompt_topic)
-            approch_tag = self.llm.query(prompt_approch)
-            problems_tag = self.llm.query(prompt_problems)
-            implementation_tag = self.llm.query(prompt_implementation)
-            algorithms_tag = self.llm.query(prompt_algorithms)
-            stage_tag = self.llm.query(prompt_stage)
 
             new_paper = paper.copy()
-            new_paper["tags"] = {"topic": topic_tag,
-                                "approach": approch_tag,
-                                "problems": problems_tag,
-                                "implementation": implementation_tag,
-                                "algorithms": algorithms_tag,
-                                "stage": stage_tag}
+            new_paper["tags"] = {"topic": topic_tag,}
             self.paper_to.send(new_paper)
 
         self.paper_from.register_listener(self, tag_arxiv)
