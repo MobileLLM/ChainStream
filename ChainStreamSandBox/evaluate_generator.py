19e49cb8f222871b1ce121282fba3ebb85136f93
Liangyan Li <1500641972@qq.com>
2024-06-24 21:48:45 +0800

    use json files to reserve output

--- ChainStreamSandBox/evaluate_generator.py
+++ ChainStreamSandBox/evaluate_generator.py
@@ -13,45 +13,6 @@ def evaluate_generator():
         task_description = task_config.task_description
         agent_code = AgentGenerator().generate_dsl(task_description)
         print(agent_code)
-#         agent_code = '''
-# import chainstream as cs
-# from chainstream.llm import get_model
-#
-# class TestAgent(cs.agent.Agent):
-#     def __init__(self):
-#         super().__init__("test_arxiv_agent")
-#         self.input_stream = cs.get_stream("all_arxiv")
-#         self.output_stream = cs.get_stream("cs_arxiv")
-#         self.llm = get_model(["text"])
-#         self.output_items = []
-#
-#     def start(self):
-#         def process_paper(paper):
-#             if "abstract" in paper:
-#                 paper_title = paper["title"]
-#                 paper_content = paper["abstract"]
-#                 paper_versions = paper["versions"]
-#                 stage_tags = ['Conceptual', 'Development', 'Testing', 'Deployment', 'Maintenance','Other']
-#                 prompt = "Give you an abstract of a paper: {} and the version of this paper: {}. What tag would you like to add to this paper? Choose from the following: {}".format(paper_content, paper_versions, ', '.join(stage_tags))
-#                 prompt_message = [
-#                     {
-#                         "role": "user",
-#                         "content": prompt
-#                     }
-#                 ]
-#                 response = self.llm.query(prompt_message)
-#                 print(paper_title + " : " + response)
-#                 self.output_stream.add_item(paper_title + " : " + response)
-#                 self.output_items.append(paper_title + " : " + response)
-#         self.input_stream.register_listener(self, process_paper)
-#
-#     def get_output_items(self):
-#         return self.output_items
-#
-#     def stop(self):
-#         self.input_stream.unregister_listener(self)
-# '''
-
         oj = SandBox(task_config, agent_code)
         try:
             oj.start_test_agent()
@@ -59,18 +20,6 @@ def evaluate_generator():
         except Exception as e:
             print(f"Error occurred while testing agent for task {task_name}: {e}")
             res = {"error": str(e)}
-
-        agent = oj.get_agent()
-        if agent is not None and hasattr(agent, 'get_output_items'):
-            output_items = agent.get_output_items()
-            if output_items is None:
-                output_items = []
-            res['output_items'] = output_items
-        else:
-            res['output_items'] = []
-
-        all_results.append(res)
-
         with open(f"{results_dir}/{task_name}.json", "w") as f:
             json.dump(res, f)
 
