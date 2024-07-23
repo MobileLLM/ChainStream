5266e614bb4c293f580beaae151bedcd05464b9c
Liangyan Li <1500641972@qq.com>
2024-07-23 20:14:53 +0800

    some changes for testing react generator on success rate

--- ChainStreamSandBox/scripts/eval_few_shot_generator.py
+++ ChainStreamSandBox/scripts/eval_few_shot_generator.py
@@ -1,6 +1,6 @@
-from sandbox_batch_interface import SandboxBatchInterface
+from ChainStreamSandBox.sandbox_batch_interface import SandboxBatchInterface
 from AgentGenerator import FewShotGenerator
-
+from ChainStreamSandBox.tasks import get_task_batch
 
 class FewShotEvaluator(SandboxBatchInterface):
     def __init__(self, task_list, repeat_time=5, result_path='./result', task_log_path=None):
@@ -9,5 +9,9 @@ class FewShotEvaluator(SandboxBatchInterface):
     def get_agent_for_specific_task(self, task) -> str:
         generator = FewShotGenerator()
         # TODO: fix this para with a new output description
-        agent = generator.generate_agent(task, task)
+        agent = generator.generate_agent(task.output_stream_description, task.input_stream_description)
         return agent
+if __name__ == '__main__':
+    task_list = get_task_batch()
+    evaluator = FewShotEvaluator(task_list)
+    evaluator.start()
