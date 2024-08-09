550694457756e43bee57484d2730a49a9fda6ee1
Liangyan Li <1500641972@qq.com>
2024-08-09 22:12:09 +0800

    update eval react scripts

--- ChainStreamSandBox/scripts/eval_ourwork_generator.py
+++ ChainStreamSandBox/scripts/eval_ourwork_generator.py
@@ -1,13 +1,17 @@
-from sandbox_batch_interface import SandboxBatchInterface
-from AgentGenerator import ReactPlusGenerator
-
+from ChainStreamSandBox.sandbox_batch_interface import SandboxBatchInterface
+from AgentGenerator import ReactGeneratorForDebugging
+from ChainStreamSandBox.tasks import get_task_batch
 
 class EvalReactPlusGenerator(SandboxBatchInterface):
     def __init__(self, task_list, repeat_time=5, result_path='./result', task_log_path=None):
         super().__init__(task_list, repeat_time, result_path, task_log_path)
 
     def get_agent_for_specific_task(self, task) -> str:
-        generator = ReactPlusGenerator()
+        generator = ReactGeneratorForDebugging()
         # TODO: fix this para with a new output description
-        agent = generator.generate_agent(task, task)
+        agent = generator.generate_agent(task.output_stream_description, task.input_stream_description)
         return agent
+if __name__ == '__main__':
+    task_list = get_task_batch()
+    evaluator = EvalReactPlusGenerator(task_list)
+    evaluator.start()
--- ChainStreamSandBox/scripts/eval_react_generator.py
+++ ChainStreamSandBox/scripts/eval_react_generator.py
@@ -1,14 +1,14 @@
 from ChainStreamSandBox.sandbox_batch_interface import SandboxBatchInterface
-from AgentGenerator import ReactGenerator
+from AgentGenerator import ReactGeneratorForStarting
 from ChainStreamSandBox.tasks import get_task_batch
 
 
 class EvalReactGenerator(SandboxBatchInterface):
-    def __init__(self, task_list, repeat_time=3, result_path='./result', task_log_path=None):
+    def __init__(self, task_list, repeat_time=2, result_path='./result', task_log_path=None):
         super().__init__(task_list, repeat_time, result_path, task_log_path)
 
     def get_agent_for_specific_task(self, task) -> str:
-        generator = ReactGenerator()
+        generator = ReactGeneratorForStarting()
         # TODO: fix this para with a new output description
         agent = generator.generate_agent(task.output_stream_description, task.input_stream_description)
         return agent
