bb2b38ef1fad961fce054a3cbc7ef538d6710749
Liangyan Li <1500641972@qq.com>
2024-09-01 14:40:18 +0800

    update gpt key and url

--- AgentGenerator/utils/llm_utils.py
+++ AgentGenerator/utils/llm_utils.py
@@ -15,8 +15,8 @@ from PIL import Image
 logger = logging.getLogger(__name__)
 
 GPT_CONFIG = {
-    "url": "https://tbnx.plus7.plus/v1",
-    "key": "sk-Eau4dcC9o9Bo1N3ID4EcD394F15b4c029bBaEfA9D06b219b"
+    "url": "https://api.openai-proxy.org",
+    "key": "sk-43Kn6GuGNxD0KwGB1XgiEyQ8htVDan44XXdnQqXA7VkZ7sMI"
 }
 
 
