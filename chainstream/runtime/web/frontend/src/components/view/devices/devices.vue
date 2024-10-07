d3bc74c2d69ad41a524e546c521d7ea2bb920261
liou <jia.cheng.liu@qq.com>
2024-10-07 14:42:30 +0800

    update devices page

--- chainstream/runtime/web/frontend/src/components/view/devices/devices.vue
+++ chainstream/runtime/web/frontend/src/components/view/devices/devices.vue
@@ -32,9 +32,10 @@
       </el-form-item>
     </el-form>
 
-    <h2>Sensor information</h2>
-    <el-form v-for="(sensor, index) in sensorForm" :key="index" :model="sensor" :inline="true">
-      <el-form-item label="Name">
+    <h2 v-if="isNewDeviceChecked">Sensor information</h2>
+    <el-form v-if="isNewDeviceChecked" v-for="(sensor, index) in sensorForm" :key="index" :model="sensor">
+      <h3>Sensor {{ index }}</h3>
+      <el-form-item :label="'Name ' + index" size="small">
         <el-input v-model="sensor.name" autocomplete="off" />
       </el-form-item>
 <!--      <el-form-item label="Device type">-->
@@ -43,17 +44,25 @@
 <!--          <el-option label="Linux device" value="linux" />-->
 <!--        </el-select>-->
 <!--      </el-form-item>-->
-      <el-form-item label="Type">
+      <el-form-item :label="'Type ' + index" size="small">
         <el-input v-model="sensor.type" autocomplete="off" />
       </el-form-item>
 <!--      <el-form-item label="Agent">-->
 <!--        <el-input v-model="sensor.agent" autocomplete="off" />-->
 <!--      </el-form-item>-->
-      <el-form-item label="Agent">
-        <el-select v-model="sensor.agent">
-          <el-option label="Android device" value="android" />
-          <el-option label="Linux device" value="linux" />
+      <el-form-item :label="'Agent ' + index" size="small">
+        <el-select v-model="sensor.agent" placeholder="Select">
+<!--          <el-option label="Android device" value="android" />-->
+<!--          <el-option label="Linux device" value="linux" />-->
+          <el-option v-for="(agent, index) in agent_list" :key="index" :label="agent.label" :value="agent.value"></el-option>
         </el-select>
+<!--        <el-cascader-->
+<!--          v-model="agent_list"-->
+<!--          :options="agent_list"-->
+<!--          :props="props"-->
+<!--          @change="handleChange"-->
+<!--          clearable-->
+<!--        />-->
       </el-form-item>
     </el-form>
 
@@ -73,7 +82,7 @@
   <el-container>
     <el-row style=" width: 100%; margin: 0; padding: 0" align="middle" justify="start">
       <!-- 使用v-for循环遍历cards数组 -->
-      <el-col v-for="(card, index) in cards" :key="index" :span="6" style="align-content: center; justify-content: center;">
+      <el-col v-for="(card, index) in device_cards" :key="index" :span="6" style="align-content: center; justify-content: center;">
         <device_card :model_name="card.model_name"></device_card>
       </el-col>
     </el-row>
@@ -110,10 +119,22 @@ const sensorForm = reactive([
     },
 ])
 
-const cards = [
+const device_cards = [
   { model_name: "Phone", content: 'Content of card'},
   { model_name: "Watch", content: 'Content of card'},
   { model_name: "Edge Sensor", content: 'Content of card'}
+]
+
+const agent_list = [
+  {
+    value: 'agent1',
+    label: 'Agent 1',},
+    {
+    value: 'agent2',
+    label: 'Agent 2',},
+    {
+    value: 'agent3',
+    label: 'Agent 3',},
 ]
 
 const resetForm = () => {
